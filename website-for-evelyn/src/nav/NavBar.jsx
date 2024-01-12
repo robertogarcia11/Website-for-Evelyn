d8db0147d187c633c60fe8f7af84a44e4f88a2a4
Roberto Garcia <129777137+robertogarcia11@users.noreply.github.com>
2024-01-12 08:35:51 +0000

    navbar update

--- website-for-evelyn/src/nav/NavBar.jsx
+++ website-for-evelyn/src/nav/NavBar.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import "../styles/NavBar.css";
 
 function NavBar() {
-    return (
-        <nav className="navbar">
-            <div className='navbar-logo'>
-                <img src='src/images/flower.png' alt="logo" />
-            </div>
 
-            <ul className='navbar-menu'>
+    const [active, setActive] = useState("navbar-menu");
+
+    const navToggle = () => {
+        active === 'navbar-menu' 
+            ? setActive('navbar-menu nav-active') 
+            : setActive('navbar-menu');
+    }
+
+    return (
+        <nav className="nav">
+            
+            <a href="#" className="nav-logo">
+                <img src='src/images/flower.png' alt="logo" width="150" heigh="150"/>
+            </a>
+
+            <ul className={active}>
                 <li className='navbar-item'><a href='/'>Home</a></li>
                 <li className='navbar-item'><a href='/policies'>Policies</a></li>
                 <li className='navbar-item'><a href='/requests'>Requests</a></li>
                 <li className='navbar-item'><a href='/socials'>Socials</a></li>
             </ul>
 
-            <div className="nav-toggler">
+            <div onClick={navToggle} className="nav-toggler">
                 <div className="line1"></div>
                 <div className="line2"></div>
                 <div className="line3"></div>
